b9b9eaf10c5c8fa982a528a2716168ff24fc4228
mrchappell <michael.r.chappell@gmail.com>
2019-12-02 20:49:42 -0600

    Presentational and Container Components

--- nucampsite/src/components/CampsiteInfoComponents.js
+++ nucampsite/src/components/CampsiteInfoComponents.js
@@ -24,7 +24,7 @@ class CampsiteInfo extends Component {
                 <div className="col-md-5 m-1">
                     <h4>Comments</h4>
                     {comments.map(comment => {
-                        return (<div key={comment.id} > {comment.text}<br/>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}<br/><br/></div>);
+                        return (<div key={comment.id} > {comment.text}<br />-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}<br /><br /></div>);
                     }
                     )
                     }
@@ -41,10 +41,12 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)};
-                    {this.renderComments(this.props.campsite.comments)};
+                <div className="container" >
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
                 </div>
+                </div >
             );
         }
         return (
