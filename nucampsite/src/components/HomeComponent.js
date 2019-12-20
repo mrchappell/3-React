87b08c6c4edb1c6a3faf6fde694c7db5223be612
mrchappell <michael.r.chappell@gmail.com>
2019-12-20 00:23:46 -0600

    Fetch from Server

--- nucampsite/src/components/HomeComponent.js
+++ nucampsite/src/components/HomeComponent.js
@@ -31,13 +31,15 @@ function Home(props) {
             <div className="row">
                 <div className="col-md m-1">
                     <RenderCard
-                        item={props.promotion}
-                        isLoading={props.promotionLoading}
-                        errMess={props.promotionErrMess}
+                        item={props.campsite}
+                        isLoading={props.campsitesLoading}
+                        errMess={props.campsitesErrMess}
                     />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion}
+                                isLoading={props.promotionLoading}
+                                errMess={props.promotionErrMess} />
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.partner} />
