d5d33608ecee890ba15bfa3c9809d826d0d81401
mrchappell <michael.r.chappell@gmail.com>
2019-12-20 00:56:42 -0600

    Fetch Post Comment

--- nucampsite/src/components/CampsiteInfoComponents.js
+++ nucampsite/src/components/CampsiteInfoComponents.js
@@ -24,7 +24,7 @@ class CommentForm extends Component {
 
     handleSubmit(values) {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
 
     toggleModal() {
@@ -114,7 +114,7 @@ function RenderCampsite({ campsite }) {
     );
 }
 
-function RenderComments({ comments, addComment, campsiteId }) {
+function RenderComments({ comments, postComment, campsiteId }) {
     if (comments) {
         return (
             <div className="col-md-5 m-1">
@@ -125,7 +125,7 @@ function RenderComments({ comments, addComment, campsiteId }) {
                         <br />
                     </div>);
                 })}
-                <CommentForm campsiteId = {campsiteId} addComment = {addComment} />
+                <CommentForm campsiteId = {campsiteId} postComment = {postComment} />
             </div>
 
         );
@@ -175,7 +175,7 @@ function CampsiteInfo(props) {
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments}
-                                    addComment = {props.addComment}
+                                    postComment = {props.postComment}
                                     campsiteId = {props.campsite.id}
                                     />
 
